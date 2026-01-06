686be1f5bd3aec5726de842f34c8610db82d85f0
Giovani Moura <thegiovanimoura@gmail.com>
2026-01-06 17:58:54 -0300

    Funcionalidade de Implementa funcionalidade de depósito no caixa eletrônico

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+
+// Estado inicial do Caixa Eletronico 
+
+//Saldo Inicial 
+let saldo = 0;
+
+//Histórico de operações (Extrato)
+let extrato = [];
+
+const saldoElemento = document.getElementById('saldo');
+const inputValor = document.getElementById('valor');
+const btnDepositar = document.getElementById('btn-depositar');
+const btnSacar = document.getElementById('btn-sacar');
+
+function atualizarSaldo(){
+  saldoElemento.textContent = `R$ ${saldo.toFixed(2)}`;
+}
+
+atualizarSaldo();
+
+// Evento de deposito 
+
+btnDepositar.addEventListener('click', function(){
+  const valor = Number(inputValor.value);
+
+  if (valor <= 0 || isNaN(valor)){
+    alert('Digite um valor válido para depósito.');
+    return;
+  }
+
+  saldo += valor;
+
+  extrato.push(`Depósito: + R$ ${valor.toFixed(2)}`);
+
+  atualizarSaldo();
+
+  inputValor.value = '';
+})
